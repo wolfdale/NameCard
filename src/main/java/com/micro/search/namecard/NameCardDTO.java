8471b736ffa4c374e70773937296512d7032039a
Prakhar Gupta <mywolfdale@gmail.com>
2020-05-20 21:05:49 +0530

    Init Spring Data ElasticSearch v4.0
    Spring Data (Elastic Search)

---
+++ src/main/java/com/micro/search/namecard/NameCardDTO.java
@@ -0,0 +1,41 @@
+package com.micro.search.namecard;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Document(indexName = "namecard", createIndex = true, replicas = 3, shards = 1)
+public class NameCardDTO {
+    @Field(type = FieldType.Text)
+    private String name;
+
+    @Field(type = FieldType.Long)
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    NameCardDTO(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "[NameCard] With Name " + this.name + " Age : " + this.age;
+    }
+}
